cfae24e4586ef9f6be445d156291b1a19b20da64
larissabrisola <alarissabrisola@gmail.com>
2024-06-23 15:57:18 -0300

    add casos

--- backend/cypress/e2e/users/createUser.cy.js
+++ backend/cypress/e2e/users/createUser.cy.js
@@ -1,5 +1,9 @@
 import { faker } from "@faker-js/faker";
 
+// 4 - compliance stations
+// 5 - contato seguro 
+// 6 - compliance total
+
 describe("Criar usuário", () => {
   let validUser = {
     name: faker.person.firstName(),
@@ -7,7 +11,7 @@ describe("Criar usuário", () => {
     telephone: faker.phone.number(),
     birth_date: faker.date.birthdate(),
     birth_city: faker.location.city(),
-    companies: [6],
+    companies: ['6'],
   };
 
   //os primeiros dois testes quebram pelo status code incorreto mas a criação do usuário é realizada com sucesso
@@ -24,7 +28,12 @@ describe("Criar usuário", () => {
         companies: validUser.companies,
       },
     }).then((response) => {
+      expect(response.body.name).to.deep.equal(validUser.name)
+      expect(response.body.email).to.deep.equal(validUser.email)
+      expect(response.body.telephone).to.deep.equal(validUser.telephone)
+      expect(response.body.birth_city).to.deep.equal(validUser.birth_city)
       expect(response.status).to.equal(201);
+
     });
   });
 
@@ -40,9 +49,56 @@ describe("Criar usuário", () => {
         companies: validUser.companies,
       },
     }).then((response) => {
+      expect(response.body.name).to.deep.equal(validUser.name)
+      expect(response.body.email).to.deep.equal(validUser.email)
+      expect(response.body.telephone).to.deep.equal(validUser.telephone)
       expect(response.status).to.equal(201);
     });
   });
+
+
+  it('Deve ser possivel criar usuário informando 2 empresas', ()=>{
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8400/api/user/create",
+      body: {
+        name: validUser.name,
+        email: validUser.email,
+        telephone: validUser.telephone,
+        birth_date: validUser.birth_date,
+        birth_city: validUser.birth_city,
+        companies: ['5', '6'],
+      },
+    }).then((response) => {
+      expect(response.body.name).to.deep.equal(validUser.name)
+      expect(response.body.email).to.deep.equal(validUser.email)
+      expect(response.body.telephone).to.deep.equal(validUser.telephone)
+      expect(response.body.birth_city).to.deep.equal(validUser.birth_city)
+      expect(response.status).to.equal(201);
+    });
+  })
+
+  
+  it('Deve ser possivel criar usuário informando 3 empresas', ()=>{
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8400/api/user/create",
+      body: {
+        name: validUser.name,
+        email: validUser.email,
+        telephone: validUser.telephone,
+        birth_date: validUser.birth_date,
+        birth_city: validUser.birth_city,
+        companies: ['4', '5', '6'],
+      },
+    }).then((response) => {
+      expect(response.body.name).to.deep.equal(validUser.name)
+      expect(response.body.email).to.deep.equal(validUser.email)
+      expect(response.body.telephone).to.deep.equal(validUser.telephone)
+      expect(response.body.birth_city).to.deep.equal(validUser.birth_city)
+      expect(response.status).to.equal(201);
+    });
+  })
 
   it("Não deve ser possivel criar usuário sem informar nome", () => {
     cy.request({
